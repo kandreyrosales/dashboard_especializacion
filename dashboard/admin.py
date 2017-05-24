d02587fd16aacacfb0bf9388b8ba4c18b4508543
kandreyrosales <kandreyrosales@gmail.com>
2017-05-24 02:03:12 -0500

    changes

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,12 +3,21 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from dashboard.models import *
 
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'codigo', 'precio', 'cantidad', 'categoria')
+
+
+class CotizacionAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'medio_de_pago', 'fecha_cotizacion', 'total_cotizacion', 'pagado')
+
+
 # Register your models here.
 admin.site.register(Ciudad)
 admin.site.register(Cliente)
 admin.site.register(Categoria)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Vendedor)
-admin.site.register(Cotizacion)
+admin.site.register(Cotizacion, CotizacionAdmin)
 admin.site.register(Area)
 admin.site.register(Presupuesto)
--- dashboard/models.py
+++ dashboard/models.py
@@ -2,14 +2,18 @@
 from __future__ import unicode_literals
 from django.db import models
 
-PAISES = [('Colombia', 'Colombia'), ('Argentina', 'Argentina'), ('España', 'España'), ('Brasil', 'Brasil'),
-          ('Venezuela', 'Venezuela'), ('Ecuador', 'Ecuador')]
+PAISES = [('Colombia', 'Colombia')]
+
+DEPARTAMENTOS = [('Cesar', 'Cesar'),
+                 ('Atlántico', 'Atlántico'),
+                 ('La Guajira', 'La Guajira'), ('Magdalena', 'Magdalena'),
+                 ('Bolivar', 'Bolivar'), ('Córdoba', 'Córdoba')]
 
 
 class Ciudad (models.Model):
     nombre = models.CharField(max_length=50, primary_key=True)
     pais = models.CharField(max_length=20, choices=PAISES)
-    departamento = models.CharField(max_length=50)
+    departamento = models.CharField(max_length=20, choices=DEPARTAMENTOS)
 
     def __unicode__(self):
         return unicode(self.nombre)
@@ -20,7 +24,7 @@ class Cliente (models.Model):
     nombres = models.CharField(max_length=256)
     apellidos = models.CharField(max_length=256)
     edad = models.IntegerField()
-    sexo = models.CharField(max_length=1, choices=[('M','M'), ('F','F')])
+    sexo = models.CharField(max_length=1, choices=[('M', 'M'), ('F', 'F')])
     fecha_nacimiento = models.DateField()
     ciudad_nacimiento = models.ForeignKey(Ciudad)
     direccion = models.CharField(max_length=256)
@@ -61,15 +65,16 @@ class Vendedor (models.Model):
 
 
 class Cotizacion (models.Model):
-    fecha_cotizacion = models.DateTimeField(auto_now_add=True, blank=True)
+    fecha_cotizacion = models.DateTimeField(auto_now_add=True, blank=False)
     precio = models.DecimalField(max_digits=10, decimal_places=3)
     iva = models.DecimalField(max_digits=10, decimal_places=1)
     total_cotizacion = models.DecimalField(max_digits=15, decimal_places=2)
     vendedor = models.ForeignKey(Vendedor)
     cliente = models.ForeignKey(Cliente)
-    medio_de_pago = models.CharField(max_length=50, choices=[(1,'Debito'), (2,'Credito'), (3,'Efectivo')])
-    pagado = models.BooleanField()
-    descuento = models.DecimalField(max_digits=2, decimal_places=1)
+    medio_de_pago = models.CharField(max_length=50, choices=[('No aplica', 'No aplica'), ('Debito', 'Debito'),
+                                                             ('Credito', 'Credito'), ('Efectivo', 'Efectivo')])
+    pagado = models.CharField(max_length=2, choices=[('Si','Si'), ('No', 'No')])
+    descuento = models.DecimalField(max_digits=3, decimal_places=1)
 
     def __unicode__(self):
         return unicode(u'{0} {1} - {2}'.format(self.cliente.identificacion, self.medio_de_pago, self.pagado))
@@ -85,7 +90,7 @@ class Area (models.Model):
 
 class Presupuesto (models.Model):
     fecha_presupuesto = models.DateField()
-    cantidad_asignada = models.DecimalField(max_digits=10, decimal_places=3, blank=True)
+    cantidad_asignada = models.DecimalField(max_digits=15, decimal_places=3, blank=True)
     vigencia = models.IntegerField()  # vigencia en meses
     area = models.ForeignKey(Area)
 
